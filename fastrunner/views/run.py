ac346743083bea81d2bd9c1e0ad2eeeadff8cc22
尹全旺 <m17602144093@163.com>
2018-12-06 13:51:06 +0800

    分拆views视图

---
+++ fastrunner/views/run.py
@@ -0,0 +1,113 @@
+from rest_framework.decorators import api_view
+from fastrunner.utils import loader
+from rest_framework.response import Response
+from fastrunner.utils.parser import Format
+from fastrunner import models
+
+"""运行方式
+"""
+
+
+@api_view(['POST'])
+def run_api(request):
+    """ run api by body and config
+    """
+    config = request.data.pop("config")
+    api = Format(request.data)
+    api.parse()
+
+    summary = loader.debug_api(api.testcase, config, api.project)
+
+    return Response(summary)
+
+
+@api_view(['GET'])
+def run_api_pk(request, **kwargs):
+    """run api by pk and config
+    """
+    api = models.API.objects.get(id=kwargs['pk'])
+    testcase = eval(api.body)
+
+    summary = loader.debug_api(testcase, request.query_params["config"], api.project.id)
+
+    return Response(summary)
+
+
+@api_view(['POST'])
+def run_api_tree(request):
+    """run api by tree
+    {
+        project: int
+        relation: list
+        config: int
+    }
+    """
+    # order by id default
+    project = request.data['project']
+    relation = request.data["relation"]
+
+    testcase = []
+    for relation_id in relation:
+        api = models.API.objects.filter(project__id=project, relation=relation_id).order_by('id').values('body')
+        for content in api:
+            testcase.append(eval(content['body']))
+
+    summary = loader.debug_api(testcase, request.data["config"], project)
+
+    return Response(summary)
+
+
+@api_view(["POST"])
+def run_testsuite(request):
+    """debug testsuite
+    {
+        name: str,
+        config: int
+        body: dict
+    }
+    """
+    body = request.data["body"]
+
+    testcase_list = []
+
+    for test in body:
+        testcase_list.append(loader.load_test(test))
+
+    summary = loader.debug_api(testcase_list, request.data['config'], request.data["project"])
+
+    return Response(summary)
+
+
+@api_view(["POST"])
+def run_test(request):
+    """debug single test
+    {
+        config: int
+        body: dict
+    }
+    """
+
+    body = request.data["body"]
+    summary = loader.debug_api(loader.load_test(body), request.data["config"], request.data["project"])
+    return Response(summary)
+
+
+@api_view(["GET"])
+def run_testsuite_pk(request, **kwargs):
+    """run testsuite by pk
+        pk: int
+        config: int
+    """
+    pk = kwargs["pk"]
+
+    test_list = models.CaseStep.objects. \
+        filter(case__id=pk).order_by("step").values("body")
+
+    testcase_list = []
+
+    for content in test_list:
+        testcase_list.append(eval(content["body"]))
+
+    summary = loader.debug_api(testcase_list, request.query_params["config"], request.query_params["project"])
+
+    return Response(summary)
---
+++ fastrunner/views/suite.py
@@ -0,0 +1,171 @@
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
+from fastrunner import models, serializers
+
+from rest_framework.response import Response
+from fastrunner.utils import response
+from fastrunner.utils import prepare
+
+
+class TestCaseView(GenericViewSet):
+    queryset = models.Case.objects
+    serializer_class = serializers.CaseSerializer
+
+    def get(self, request):
+        """
+        查询指定CASE列表，不包含CASE STEP
+        {
+            "project": int,
+            "node": int
+        }
+        """
+        node = request.query_params["node"]
+        project = request.query_params["project"]
+
+        # update_time 降序排列
+        queryset = self.get_queryset().filter(project__id=project, relation=node).order_by('-update_time')
+
+        pagination_query = self.paginate_queryset(queryset)
+        serializer = self.get_serializer(pagination_query, many=True)
+
+        return self.get_paginated_response(serializer.data)
+
+    def copy(self, request, **kwargs):
+        """
+        pk int: test id
+        {
+            name: test name
+            relation: int
+            project: int
+        }
+        """
+        pk = kwargs['pk']
+
+        if models.Case.objects.filter(**request.data).first():
+            return Response(response.CASE_EXISTS)
+
+        case = models.Case.objects.get(id=pk)
+        case.id = None
+        case.name = request.data['name']
+        case.save()
+
+        case_step = models.CaseStep.objects.filter(case__id=pk)
+
+        for step in case_step:
+            step.id = None
+            step.case = models.Case.objects.get(name=request.data['name'])
+            step.save()
+
+        return Response(response.CASE_ADD_SUCCESS)
+
+    def patch(self, request, **kwargs):
+        """
+        更新测试用例集
+        {
+            name: str
+            id: int
+            body: []
+        }
+        """
+
+        pk = kwargs['pk']
+
+        body = request.data.pop('body')
+
+        if "case" in body[0].keys():
+            case_info = body[0]["case"]
+        else:
+            case_info = body[0]
+
+        if models.Case.objects.exclude(id=pk). \
+                filter(name=request.data['name'],
+                       project__id=case_info["project"],
+                       relation=case_info["relation"]).first():
+            return Response(response.CASE_EXISTS)
+
+        case = models.Case.objects.get(id=pk)
+
+        prepare.update_casestep(body, case)
+
+        models.Case.objects.filter(id=pk).update(**request.data)
+
+        return Response(response.CASE_UPDATE_SUCCESS)
+
+    def post(self, request):
+        """
+        新增测试用例集
+        {
+            name: str
+            project: int,
+            relation: int,
+            body: [{
+                id: int,
+                project: int,
+                name: str,
+                method: str,
+                url: str
+            }]
+        }
+        """
+
+        try:
+            pk = request.data['project']
+            request.data['project'] = models.Project.objects.get(id=pk)
+
+        except KeyError:
+            return Response(response.KEY_MISS)
+
+        except ObjectDoesNotExist:
+            return Response(response.PROJECT_NOT_EXISTS)
+
+        body = request.data.pop('body')
+
+        # 同一项目同一节点下存在相同用例集
+        if models.Case.objects.filter(**request.data).first():
+            return Response(response.CASE_EXISTS)
+
+        models.Case.objects.create(**request.data)
+
+        case = models.Case.objects.filter(**request.data).first()
+
+        prepare.generate_casestep(body, case)
+
+        return Response(response.CASE_ADD_SUCCESS)
+
+    def delete(self, request, **kwargs):
+        """
+        pk: test id delete single
+        [{id:int}] delete batch
+        """
+        pk = kwargs.get('pk')
+
+        try:
+            if pk:
+                prepare.case_end(pk)
+            else:
+                for content in request.data:
+                    prepare.case_end(content['id'])
+
+        except ObjectDoesNotExist:
+            return Response(response.SYSTEM_ERROR)
+
+        return Response(response.CASE_DELETE_SUCCESS)
+
+
+class CaseStepView(APIView):
+    """
+    测试用例step操作视图
+    """
+
+    def get(self, request, **kwargs):
+        """
+        返回用例集信息
+        """
+        pk = kwargs['pk']
+
+        queryset = models.CaseStep.objects.filter(case__id=pk).order_by('step')
+
+        serializer = serializers.CaseStepSerializer(instance=queryset, many=True)
+
+        return Response(serializer.data)
