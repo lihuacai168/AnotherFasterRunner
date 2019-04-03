251cbe254ab79cbbc14b1e5349b0ff357ca0c04a
尹全旺 <32924136+yinquanwang@users.noreply.github.com>
2019-04-03 14:09:33 +0800

    Update prepare.py

--- fastrunner/utils/prepare.py
+++ fastrunner/utils/prepare.py
@@ -23,7 +23,6 @@ def get_project_detail(pk):
     variables_count = get_counter(models.Variables, pk=pk)
     report_count = get_counter(models.Report, pk=pk)
     host_count = get_counter(models.HostIP, pk=pk)
-    plan_count = get_counter(models.Plan, pk=pk)
     task_count = celery_models.PeriodicTask.objects.filter(description=pk).count()
 
     return {
@@ -33,8 +32,7 @@ def get_project_detail(pk):
         "config_count": config_count,
         "variables_count": variables_count,
         "report_count": report_count,
-        "host_count":host_count,
-        "plan_count": plan_count
+        "host_count":host_count
     }
 
 
