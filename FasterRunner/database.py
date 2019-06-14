29fdb27209ef0f2f553f26bed09385cd4373afa3
Rikasai_wbiao <lihuacai@wbiao.cn>
2019-06-14 14:44:04 +0800

    1.自动选择数据库调试-1

--- FasterRunner/database.py
+++ FasterRunner/database.py
@@ -10,17 +10,19 @@ import platform
 
 
 class AutoChoiceDataBase:
-    run_system = platform.system()
-    is_windows = run_system == 'Windows'
 
     def db_for_read(self, model, **hints):
-        if self.is_windows:
+        run_system = platform.system()
+        is_windows = run_system == 'Windows'
+        if is_windows:
             return 'default'
         else:
             return 'remote'
 
     def db_for_write(self, model, **hints):
-        if self.is_windows:
+        run_system = platform.system()
+        is_windows = run_system == 'Windows'
+        if is_windows:
             return 'default'
         else:
             return 'remote'
