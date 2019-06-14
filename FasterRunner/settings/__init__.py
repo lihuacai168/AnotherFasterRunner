f740f95701be1c4a9b36001d62864711028de7c1
Rikasai_wbiao <lihuacai@wbiao.cn>
2019-06-14 17:53:18 +0800

    1.区分本地和生产模式启动

---
+++ FasterRunner/settings/__init__.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Author:梨花菜
+# @File: __init__.py.py 
+# @Time : 2019/6/14 17:05
+# @Email: lihuacai168@gmail.com
+# @Software: PyCharm
---
+++ FasterRunner/settings/pro.py
@@ -0,0 +1,15 @@
+from .base import *
+DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '10.0.3.57',
+        'NAME': 'fast_last',  # 新建数据库名
+        'USER': 'faster',  # 数据库登录名
+        'PASSWORD': 'fast!~WB2019',  # 数据库登录密码
+        'TEST': {
+            # 'MIRROR': 'default',  # 单元测试时,使用default的配置
+            # 'DEPENDENCIES': ['default']
+        }
+    }
+}
