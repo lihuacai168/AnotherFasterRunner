e713196ecbd01d269a3bdae7e4b8f5608d3dfbfb
faster <lihuacai168@gmail.com>
2019-06-15 15:10:44 +0800

    修改uwsgi配置

--- FasterRunner/wsgi.py
+++ FasterRunner/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FasterRunner.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FasterRunner.settings.pro')
 
 application = get_wsgi_application()
