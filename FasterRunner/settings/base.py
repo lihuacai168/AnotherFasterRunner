bef43f225a6615ad4bf5333195e702369b4ea764
Rikasai_wbiao <lihuacai@wbiao.cn>
2019-06-27 20:12:17 +0800

    1.修复测试报告找不到的错误

--- FasterRunner/settings/base.py
+++ FasterRunner/settings/base.py
@@ -63,7 +63,8 @@ ROOT_URLCONF = 'FasterRunner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../../templates')],
+        # 'DIRS': [os.path.join(BASE_DIR, '../../templates')],
+        'DIRS': [os.path.join(BASE_DIR, './templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
