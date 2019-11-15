a5d9b4e9985835b3efe35b81b3b64cda8d260ba7
Rikasai_wbiao <lihuacai@wbiao.cn>
2019-11-15 16:14:39 +0800

    1.修改数据库连接字符集为utf8mb4 2.增加开发环境的celery配置

--- FasterRunner/settings/dev.py
+++ FasterRunner/settings/dev.py
@@ -4,11 +4,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'fast',  # 新建数据库名
+        # 'NAME': 'fast_mb4',  # 新建数据库名
         'USER': 'root',  # 数据库登录名
         'PASSWORD': 'root',  # 数据库登录密码
+        'OPTIONS': {'charset': 'utf8mb4'},
         # 单元测试数据库
         'TEST': {
             'NAME': 'test_fast_last',  # 测试过程中会生成名字为test的数据库,测试结束后Django会自动删除该数据库
         }
     }
 }
+BROKER_URL = 'amqp://username:password@localhost:5672//'
--- FasterRunner/settings/pro.py
+++ FasterRunner/settings/pro.py
@@ -7,6 +7,7 @@ DATABASES = {
         'NAME': 'fast_last',  # 新建数据库名
         'USER': 'faster',  # 数据库登录名
         'PASSWORD': 'fast!~WB2019',  # 数据库登录密码
+        'OPTIONS': {'charset': 'utf8mb4'},
         'TEST': {
             # 'MIRROR': 'default',  # 单元测试时,使用default的配置
             # 'DEPENDENCIES': ['default']
