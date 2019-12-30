7a7f703f3fb1a6d60cb2964484453f5d4df8fe5f
Rikasai_wbiao <lihuacai@wbiao.cn>
2019-12-30 13:59:48 +0800

    1.修改docker启动celery

--- start.sh
+++ start.sh
@@ -5,12 +5,14 @@ service nginx start
 
 # start celery worker
 #celery multi start w1 -A FasterRunner -l info --logfile=./logs/worker.log
-celery multi start w1 -A FasterRunner.mycelery -l info --logfile=./logs/worker.log
+python3 manage.py celery -A FasterRunner.mycelery worker -l info  --settings=FasterRunner.settings.pro --logfile=./logs/worker.log 2>&1 &
+
 
 # start celery beat
 #nohup python3 manage.py celery beat -l info > ./logs/beat.log 2>&1 &
 #nohup python3 manage.py FasterRunner.mycelery beat -l info > ./logs/beat.log 2>&1 &
-nohup celery -A FasterRunner.mycelery beat -l info > ./logs/beat.log 2>&1 &
+#nohup celery -A FasterRunner.mycelery beat -l info > ./logs/beat.log 2>&1 &
+python3 manage.py  celery -A FasterRunner.mycelery beat -l info --settings=FasterRunner.settings.pro --logfile=./logs/worker.log 2>&1 &
 
 # start fastrunner
 uwsgi --ini ./uwsgi_docker.ini
