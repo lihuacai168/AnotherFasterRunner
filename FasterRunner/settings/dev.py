d832f456b9c4822d9784f44f44814b48dcbe1960
lihuacai <lihuacai168@gmail.com>
2020-09-23 22:22:32 +0800

    1.修改：开发配置修改为本地配置

--- FasterRunner/settings/dev.py
+++ FasterRunner/settings/dev.py
@@ -6,11 +6,11 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fastrunner_dev',  # 新建数据库
+        'NAME': 'fast_dev',  # 新建数据库
         # 'NAME': 'fast_mb4',  # 新建数据库名
-        'HOST': '192.168.17.107',
-        'USER': 'fastrunner',  # 数据库登录名
-        'PASSWORD': 'fastrunner2020',  # 数据库登录密码
+        'HOST': '127.0.0.1',
+        'USER': 'root',  # 数据库登录名
+        'PASSWORD': 'root',  # 数据库登录密码
         'OPTIONS': {'charset': 'utf8mb4'},
         # 单元测试数据库
         'TEST': {
