d7c344047a9a7aa1806f3cddfc14c5686b332706
lihuacai <lihuacai168@gmail.com>
2020-12-28 23:56:18 +0800

    1.feat: swagger文档run开头统一为run分组,api分组容错判断

--- FasterRunner/swagger.py
+++ FasterRunner/swagger.py
@@ -13,8 +13,11 @@ class CustomSwaggerAutoSchema(SwaggerAutoSchema):
     def get_tags(self, operation_keys=None):
         tags = super().get_tags(operation_keys)
 
-        if "api" in tags and operation_keys:
+        if "api" in tags and len(operation_keys) >= 3:
             #  `operation_keys` 内容像这样 ['v1', 'prize_join_log', 'create']
-            tags[0] = operation_keys[2]
+            if operation_keys[2].startswith('run'):
+                tags[0] = 'run'
+            else:
+                tags[0] = operation_keys[2]
 
         return tags
