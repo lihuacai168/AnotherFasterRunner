18d6f4a2f19e191f4f9800fd9bb7ad02b3a9ff05
lihuacai <lihuacai168@gmail.com>
2021-03-23 23:51:41 +0800

    feat: 管理后台新增用户所在分组

--- fastuser/admin.py
+++ fastuser/admin.py
@@ -8,7 +8,8 @@ User = get_user_model()
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'is_active')
+    list_display = ('username', 'is_active', 'belong_groups')
+
     # 编辑资料的时候显示的字段
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
@@ -17,14 +18,19 @@ class UserAdmin(BaseUserAdmin):
         #     'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
         # }),
         (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser'),
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',),
         }),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     # 新增用户需要填写的字段
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username',  'password1', 'password2', 'is_active', 'is_staff'),
+            'fields': ('username',  'password1', 'password2', 'is_active', 'is_staff', 'groups'),
         }),
     )
+    filter_horizontal = ('groups',)
+
+    def belong_groups(self, obj):
+        return ", ".join([g.name for g in obj.groups.all()])
+
+    belong_groups.short_description = '所属分组'
