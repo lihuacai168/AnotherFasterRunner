3f25150e5f741483b2e499511189f633eb78650e
lihuacai <lihuacai168@gmail.com>
2021-04-06 22:14:41 +0800

    feat: 新增gzip中间件压缩返回值

--- FasterRunner/settings/base.py
+++ FasterRunner/settings/base.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
