56728ce6c19aba437d7a8a193ca2b883cc48dd15
lihuacai <lihuacai168@gmail.com>
2021-04-24 12:37:52 +0800

    feat: 项目详情新增接口覆盖率

--- fastrunner/serializers.py
+++ fastrunner/serializers.py
@@ -3,6 +3,7 @@ import json
 import time
 
 from crontab import CronTab
+from django.db.models import Q
 from rest_framework import serializers
 from fastrunner import models
 from fastrunner.utils.parser import Parse
@@ -16,9 +17,25 @@ class ProjectSerializer(serializers.ModelSerializer):
     项目信息序列化
     """
 
+    api_cover_rate = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Project
-        fields = ['id', 'name', 'desc', 'responsible', 'update_time', 'creator', 'updater', 'yapi_openapi_token', 'yapi_base_url']
+        fields = ['id', 'name', 'desc', 'responsible', 'update_time', 'creator', 'updater', 'yapi_openapi_token', 'yapi_base_url', 'api_cover_rate']
+
+    def get_api_cover_rate(self, obj):
+        """
+        接口覆盖率，百分比后去两位小数点
+        """
+        apis = models.API.objects.filter(project_id=obj.id).values('url', 'method')
+        api_unique = {f'{api["url"]}_{api["method"]}' for api in apis}
+        case_steps = models.CaseStep.objects.filter(case__project_id=obj.id).filter(~Q(method='config')).values('url', 'method')
+        case_steps_unique = {f'{case_step["url"]}_{case_step["method"]}' for case_step in case_steps}
+        if len(api_unique) == 0:
+            return '0.00'
+        if len(case_steps_unique) > len(api_unique):
+            return '100.00'
+        return '%.2f' % (len(case_steps_unique) / len(api_unique) * 100)
 
 
 class VisitSerializer(serializers.ModelSerializer):
