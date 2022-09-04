723c472114258c9e7f103a562e684784e1b707cc
lihuacai <lihuacai168@gmail.com>
2022-09-04 23:24:10 +0800

    refactor: change extract tree service fix: get tree default is null

---
+++ fastrunner/dto/tree_dto.py
@@ -0,0 +1,24 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Author: 花菜
+# @File: tree_dto.py
+# @Time : 2022/9/4 19:05
+# @Email: lihuacai168@gmail.com
+
+from pydantic import BaseModel, Field
+
+
+class TreeUniqueIn(BaseModel):
+    project_id: int
+    type: int
+
+
+class TreeUpdateIn(BaseModel):
+    tree: list[dict] = Field(alias='body')
+
+
+class TreeOut(BaseModel):
+    tree: list[dict]
+    id: int
+    max: int
