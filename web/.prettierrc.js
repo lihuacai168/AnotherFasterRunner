7e0367e95cf55ee16a206960e7fbe9cf56bf982f
Xiong <xiongshengpu@jucaicat.com>
2022-10-18 11:21:29 +0800

    修改配置

---
+++ web/.prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120, // 单行字符数, 默认80
+  tabWidth: 2, // 缩进字节数, 默认2
+  semi: false, // 句尾使用分号, 默认true
+  singleQuote: true, // 使用单引号代替双引号, 默认false
+  trailingComma: 'none', // 多行时尽可能打印尾随逗号, 默认'none', 可选: ['none', 'es5', 'all']
+  endOfLine: 'auto' // 结束行形式, 默认'auto', 结尾是 \n \r \n\r auto
+
+  // arrowParens: 'avoid', // 在单个箭头函数参数周围加上括号, 默认'avoid', 可选: ['avoid', 'always'] avoid: 尽可能  always: 始终
+  // bracketSpacing: true, // 在对象文字中打印括号之间的空格, 默认true, 示例: true -- { name: 'abc' }  false -- {name:'abc'}
+  // useTabs: false, // 使用制表符 tab 缩进行而不是空格, 默认false
+  // bracketSpacing: true, //在对象前后添加空格-obj: { foo: bar }
+  // jsxSingleQuote: false, // 在JSX中使用单引号代替双引号, 默认false
+}
