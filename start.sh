dbe453c86d37bd922e0e308810d9029c5fa525da
Xiong <xiongshengpu@jucaicat.com>
2022-11-21 21:25:56 +0800

    修改部署方式

--- start.sh
+++ start.sh
@@ -3,12 +3,13 @@ echo "--------------start nginx--------------"
 nginx
 
 echo "--------------start app--------------"
-# /usr/local/bin/python -m gunicorn FasterRunner.wsgi_docker -b 0.0.0.0 -w 4
-uwsgi --ini ./ComposeDeploy/uwsgi_docker.ini --logto ./logs/uwsgi.log
+uwsgi --ini ./ComposeDeploy/uwsgi_docker.ini
+# uwsgi --ini ./ComposeDeploy/uwsgi_docker.ini --logto ./logs/uwsgi.log
+# /usr/local/bin/python manage.py runserver 0.0.0.0:8000 --settings=FasterRunner.settings.docker
 
 echo "--------------start celery worker--------------"
 export DJANGO_SETTINGS_MODULE=FasterRunner.settings.docker;
-nohup python -m celery -A FasterRunner.mycelery worker -l info > ./logs/worker.log 2>&1 &
+nohup python -m celery -A FasterRunner.mycelery worker -l info --uid=1000 > ./logs/worker.log 2>&1 &
 # nohup /usr/local/bin/python manage.py celery -A FasterRunner.mycelery worker -l info --settings=FasterRunner.settings.docker --logfile=./logs/worker.log 2>&1 &
 #celery multi start w1 -A FasterRunner -l info --logfile=./logs/worker.log
 #python3 manage.py celery -A FasterRunner.mycelery worker -l info -P solo --settings=FasterRunner.settings.pro --logfile=./logs/worker.log 2>&1 &
@@ -22,12 +23,6 @@ nohup python -m celery -A FasterRunner.mycelery beat -l info > ./logs/beat.log 2
 #nohup celery -A FasterRunner.mycelery beat -l info > ./logs/beat.log 2>&1 &
 #if [ -f celerybeat.pid ]; then rm celerybeat.pid;fi
 #python3 manage.py  celery -A FasterRunner.mycelery beat -l info --settings=FasterRunner.settings.pro --logfile=./logs/beat.log 2>&1 &
-
-echo "--------------start app-----------------"
-#uwsgi --ini ./uwsgi_docker.ini
-uwsgi --ini ./ComposeDeploy/uwsgi_docker.ini --logto ./logs/uwsgi.log
-#gunicorn FasterRunner.wsgi_docker -b 0.0.0.0 -w 4
-#python3 manage.py runserver --settings=FasterRunner.settings.docker
 
 
 # if [ $1 = "app" ]; then
