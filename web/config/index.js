6f237ef7b2042b379addb09d456cf3f2273db9de
lihuacai <lihuacai168@gmail.com>
2023-05-08 16:31:30 +0800

    perf(index.js): disable sourcemap

--- web/config/index.js
+++ web/config/index.js
@@ -59,7 +59,7 @@ module.exports = {
          * Source Maps
          */
 
-        productionSourceMap: true,
+        productionSourceMap: false,
         // https://webpack.js.org/configuration/devtool/#production
         devtool: '#source-map',
 
