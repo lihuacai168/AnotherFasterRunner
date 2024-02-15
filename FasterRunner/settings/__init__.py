cdfac7c7c6593719898c4e0bd137e9d99460f57f
lihuacai <lihuacai168@gmail.com>
2024-02-15 15:55:26 +0800

    fix: collect static cannot import name 'ugettext_lazy'

--- FasterRunner/settings/__init__.py
+++ FasterRunner/settings/__init__.py
@@ -14,3 +14,4 @@ django.utils.encoding.smart_text = smart_str
 
 from django.utils.translation import gettext_lazy
 django.utils.translation.ugettext = gettext_lazy
+django.utils.translation.ugettext_lazy = gettext_lazy
