a80b3d2598e164e0220ce1681f81de969f70fc8c
lihuacai <lihuacai168@gmail.com>
2024-03-01 00:07:34 +0800

    feat: return more mock api info field

--- mock/serializers.py
+++ mock/serializers.py
@@ -12,9 +12,25 @@ from .models import MockAPI, MockProject
 
 
 class MockAPISerializer(serializers.ModelSerializer):
+    project_name = serializers.CharField(source="project.project_name", read_only=True)
+
     class Meta:
         model = MockAPI
-        fields = ["project", "request_path", "request_method", "response_text", "is_active"]
+        fields = [
+            "project",
+            "project_name",
+            "request_path",
+            "request_method",
+            "response_text",
+            "is_active",
+            "api_id",
+            "api_desc",
+            "api_name",
+            "followers",
+        ]
+        read_only_fields = [
+            "api_id",
+        ]
 
 
 class MockProjectSerializer(serializers.ModelSerializer):
